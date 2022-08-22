3655ea84d722a14478128809ffb864b0c8837f17
Juliana Sau <julienyange@gmail.com>
2022-08-22 12:45:34 +0300

    Update 102-square.py

--- 0x06-python-classes/102-square.py
+++ 0x06-python-classes/102-square.py
@@ -1,84 +1,53 @@
 #!/usr/bin/python3
-
-
 class Square:
-    """Square class."""
+    """Represents a square.
+    Private instance attribute: size:
+        - property def size(self)
+        - property setter def size(self, value)
+    Instantiation with optional size.
+    Public instance method: def area(self).
+    """
 
     def __init__(self, size=0):
-        """__init__ method that sets the size of square.
-        Args:
-            size (int): size of Square
+        """Initializes the data."""
+        self.__size = size
 
-        """
-        self.size = size
+    def __eq__(self, other):
+        """Equal."""
+        if hasattr(other, 'size'):
+            return self.__size == other.__size
+        return self.__size == other
 
-    def area(self):
-        """Gets the area of the Square.
+    def __ne__(self, other):
+        """Not equal."""
+        return not self.__eq__(other)
 
-        Returns:
-            Area of squre
+    def __lt__(self, other):
+        """Less than."""
+        if hasattr(other, 'size'):
+            return self.__size < other.__size
+        return self.__size < other
 
-        """
-        return self.__size * self.__size
+    def __le__(self, other):
+        """Less than or equal."""
+        if hasattr(other, 'size'):
+            return self.__size <= other.__size
+        return self.__size <= other
 
     @property
     def size(self):
+        """Retrieves the size."""
         return self.__size
 
     @size.setter
     def size(self, value):
-        """size setter  method that sets the size of square.
-        Args:
-            value (int): size of Square
-        Raises:
-            TypeError: If `value` is not an integer.
-            ValueError: If `value` is less than 0.
-
-        """
-        if not isinstance(value, int) and not isinstance(value, float):
+        """Sets the size to a value."""
+        if not isinstance(value, int) or not isinstance(value, float):
             raise TypeError("size must be a number")
-        if value < 0:
+        elif value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
 
-    def __eq__(self, square):
-        """Implements equals"""
-        return self.area() == square.area()
-
-    def __ne__(self, square):
-        """Implements not equal"""
-        return self.area() != square.area()
-
-    def __gt__(self, square):
-        """Implements greater than"""
-        return self.area() > square.area()
-
-    def __ge__(self, square):
-        """Implements greater or equal to"""
-        return self.area() >= square.area()
-
-    def __lt__(self, square):
-        """Implements less than"""
-        return self.area() < square.area()
-
-    def __le__(self, square):
-        """Implements less than or equal to"""
-        return self.area() <= square.area()
-
-
-if __name__ == '__main__':
-    s_5 = Square(5)
-    s_6 = Square(6)
-
-    if s_5 < s_6:
-        print("Square 5 < Square 6")
-    if s_5 <= s_6:
-        print("Square 5 <= Square 6")
-    if s_5 == s_6:
-        print("Square 5 == Square 6")
-    if s_5 != s_6:
-        print("Square 5 != Square 6")
-    if s_5 > s_6:
-        print("Square 5 > Square 6")
-    if s_5 >= s_6:
-        print("Square 5 >= Square 6")
+    def area(self):
+        """Returns the current square area."""
+        return self.__size ** 2
