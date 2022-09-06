b9a249cf069818baf2bc921d0a342ae507e79f74
Sau Juliana <julienyange@gmail.com>
2022-09-06 22:26:06 +0300

    add advanced task 1

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const list = require('./test_files/100-data').list;
+const list = require("./100-data").list;
 
 const newArray = [];
 list.map((item, index) => newArray.push(item * index));
