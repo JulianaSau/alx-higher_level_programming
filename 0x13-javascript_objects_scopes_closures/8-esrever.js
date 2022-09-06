7cbeef48f5b81c7d37d6acededc6b36ec626fcde
Sau Juliana <julienyange@gmail.com>
2022-09-06 18:51:32 +0300

    add task 8

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,5 +1,10 @@
 #!/usr/bin/node
 
 exports.esrever = function (list) {
-  return list.reverse();
+  const arr = [];
+
+  for (let i = list.length - 1; i >= 0; i--) {
+    arr.push(list[i]);
+  }
+  return arr;
 };
