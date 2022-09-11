2b19581cbf240848eaffb3d1bbcb3cb1a26eb90f
Juliana Sau <julienyange@gmail.com>
2022-09-11 13:55:49 -0700

    fix

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -16,7 +16,8 @@ if __name__ == "__main__":
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    state = session.query(State).filter(State.name == sys.argv[4]).first()
+    state = session.query(State).filter(State.name == sys.argv[4])\
+                   .order_by(State.id).first()
 
     if state:
         print("{}".format(state.id))
