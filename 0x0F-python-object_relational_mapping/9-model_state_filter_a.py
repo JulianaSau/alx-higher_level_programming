11ce6a35a5df37a087df68707fa4158584e54cc8
Juliana Sau <julienyange@gmail.com>
2022-09-11 13:42:45 -0700

    create task 10

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -8,11 +8,12 @@ from sqlalchemy import (create_engine)
 from sqlalchemy.orm import sessionmaker, Session
 
 if __name__ == "__main__":
-    eng = 'mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1], sys.argv[2],
-                                                      sys.argv[3], pool_pre_ping=True)
+    eng = 'mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1],
+                                                      sys.argv[2],
+                                                      sys.argv[3],
+                                                      pool_pre_ping=True)
     engine = create_engine(eng)
     Base.metadata.create_all(engine)
-
 
     Session = sessionmaker(bind=engine)
     session = Session()
