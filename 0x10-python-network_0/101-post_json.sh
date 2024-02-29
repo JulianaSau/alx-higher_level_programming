7f74ddff345f5a7211e30bbb2b99d70be11f0813
Juliana Sau <julienyange@gmail.com>
2024-02-29 12:06:12 -0800

    fix advanced task 5 & 8

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # sends a post request with the contents of a file passed as a file in the body of the request
-curl -sX POST -H 'Content-Type: application/json' -d @"$2" "$1"
+curl -sX POST -H 'Content-Type: application/json' -d "$(cat "$2")" "$1"
--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # script that sends a post request with params email and displays body of response
-curl -X POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
+curl -sX POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
