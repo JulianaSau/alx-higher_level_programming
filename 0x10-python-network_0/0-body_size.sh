dea9b796443ccd79549e6e5914d72aae54da719f
Juliana Sau <julienyange@gmail.com>
2024-02-29 09:54:04 -0800

    fix task 1

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,15 +1,3 @@
 #!/bin/bash
-# a Bash script that takes in a URL, sends a request to that URL, and 
-# displays the size of the body of the response
-
-if [[ $# -ne 1 ]]
-then
-	echo "Usage: 0-body_size.sh url"
-else
-	# Make GET request and store response headers in a variable
-	response_headers=$(curl -s -I $1)
-
-	# Extract the Content-Length value from the headers and print the value
-	echo "$response_headers" | grep -i "Content-Length" | awk '{print $2}'
-
-fi
+# a Bash script that takes in a URL, sends a request to that URL, and displays the size of the body of the response
+echo "$(curl -s -I $1)" | grep -i "Content-Length" | awk '{print $2}'
