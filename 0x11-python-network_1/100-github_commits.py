ca43ba2846bd647573380a6eb9dd913db1ab34a9
Juliana Sau <git config --global user.name>
2024-03-02 06:58:30 -0800

    task 10 created

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" get 10 latest comments from a repo
+    scripts takes uname and repo name
+"""
+
+import sys
+import requests
+
+if __name__ == "__main__":
+    url = "https://api.github.com/repos/{}/{}/commits".format(
+        sys.argv[1], sys.argv[2])
+    params = {'per_page': 10}
+    r = requests.get(url, params=params)
+    commits = r.json()
+    try:
+        for i in range(10):
+            print(
+                "{}: {}".format(
+                    commits[i].get("sha"),
+                    commits[i].get("commit").get("author").get("name"),
+                )
+            )
+    except(IndexError, KeyError):
+        pass
