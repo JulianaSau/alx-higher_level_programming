dccdc755a3e0110184251f53f207c14ed7dc120d
Juliana Sau <git config --global user.name>
2024-03-02 03:40:57 -0800

    fix task 0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,13 +1,16 @@
 #!/usr/bin/python3
 """ script that fetches https://alx-intranet.hbtn.io/status """
 
+
 import urllib.request
 
+
 if __name__ == "__main__":
-    req = urllib.request.Request('https://alx-intranet.hbtn.io/status')
-    with urllib.request.urlopen(req) as response:
+
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
         body = response.read()
-        print("Body Response:")
+
+        print("Body response:")
         print("\t- type: {}".format(type(body)))
         print("\t- content: {}".format(body))
-        print("\t- utf8 content: {}".format(body.decode("utf-8")))
+        print("\t- utf8 content: {}".format(body.decode('utf-8')))
