66c895f8fe2808d23eda3befa89bc46e715dc546
Juliana Sau <julienyange@gmail.com>
2024-04-08 14:11:34 +0300

    Update 4-starwars_count.js

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,26 +1,13 @@
 #!/usr/bin/node
 
-const req = require('request');
-const argsCount = process.argv.length - 2;
+const request = require('request');
+const url = process.argv[2];
 
-if (argsCount > 0) {
-  const endpoint = process.argv[2];
-
-  req(endpoint, function (err, res, body) {
-    if (err) throw err;
-    else {
-      const data = JSON.parse(body);
-      const films = data.results;
-      const filmsCount = films.length;
-      const charUrl = 'https://swapi-api.alx-tools.com/api/people/18/';
-      let presenceCount = 0;
-
-      for (let i = 0; i < filmsCount; i++) {
-        const chars = films[i].characters;
-        if (chars.includes(charUrl)) presenceCount++;
-      }
-
-      console.log(presenceCount);
-    }
-  });
-}
+request(url, (err, res, body) => {
+  if (!err) {
+    const movies = JSON.parse(body).results;
+    console.log(movies.reduce((count, movie) => {
+      return movie.characters.find((cha) => cha.endsWith('/18/')) ? count + 1 : count;
+    }, 0));
+  }
+});
